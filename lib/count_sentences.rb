c6e1ccf64104c336acfa0dbc6c99e49c2b58f5ee
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2020-09-28 01:59:51 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,8 +27,9 @@ class String
   end
 
   def count_sentences
+    #split based on RegEx params for ?, !, or .
     self.split(/[.?!]/).reject { |x| x.empty? }.length
-    #when you use reject, you're giving it the params of what to TAKE OUT.  In this case, take out empty
+      #when you use reject, you're giving it the params of what to TAKE OUT.  In this case, take out empty strings.  then use length to return the string count
       
   end
 end
